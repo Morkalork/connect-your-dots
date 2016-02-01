09f2ef5a1ef233063013d8ae05c560af4da619ae
Magnus Ferm <magnferm@axis.com>
2016-02-01 11:34:09 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+
+
+gulp.task('js', function(){
+  var b = browserify({
+    entries: './src/js/app.js',
+    debug: true,
+    transform: ['babelify']
+  });
+  
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('html', function(){
+ return gulp.src('./src/*.html')
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('css', function(){
+ return gulp.src('./src/css/*.css')
+  .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('default', ['js', 'html', 'css']);
---
+++ src/js/app.js
@@ -0,0 +1,84 @@
+var Snap = require('snapsvg');
+
+(function () {
+  const CircleRadius = 8;
+
+  var snap;
+  var lastCircle;
+
+  var circles = [];
+  var lines = [];
+
+  function render() {
+    //Guess what this does :)
+    snap.clear();
+
+    for (var i in lines) {
+      var lineInfo = lines[i];
+      snap.line(
+        lineInfo.startX,
+        lineInfo.startY,
+        lineInfo.endX,
+        lineInfo.endY
+        )
+        .attr({
+          stroke: "#000",
+          strokeWidth: 4
+        })
+        .click(function (e) {
+          console.log("line clikc!");
+          e.stopPropagation();
+        });;
+    }
+
+    for (var i in circles) {
+      var circleInfo = circles[i];
+      snap.circle(
+        circleInfo.x,
+        circleInfo.y,
+        CircleRadius
+        ).attr({
+          fill: "#bada55",
+          stroke: "#333",
+          strokeWidth: 1
+        });
+    }
+  }
+
+  function setupSnap() {
+    snap = Snap("#svg-main");
+
+    snap.click(function (e) {
+      drawCircle(e.offsetX, e.offsetY);
+      render();
+    });
+  };
+
+  function drawCircle(x, y) {
+    var newCircle = { x: x, y: y };
+    circles.push(newCircle);
+
+    if (lastCircle) {
+      addLineBetweenCircles(lastCircle, newCircle);
+    }
+
+    lastCircle = newCircle;
+  };
+
+  function addLineBetweenCircles(oldCircle, newCircle) {
+    var startX = oldCircle.x;
+    var startY = oldCircle.y;
+
+    var endX = newCircle.x;
+    var endY = newCircle.y;
+
+    var newLine = { startX: startX, startY: startY, endX: endX, endY: endY };
+    lines.push(newLine);
+  };
+
+  function init() {
+    setupSnap();
+  };
+
+  init();
+} ());
